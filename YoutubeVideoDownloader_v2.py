b983a6e3e1ff0b6dd26aa137dbd6783c07455a5b
27MrMan <128210486+27MrMan@users.noreply.github.com>
2023-04-30 15:40:32 -0400

    BUGFIX
    check the code for the bugfix. (StreamingData fix)

--- YoutubeVideoDownloader_v2.py
+++ YoutubeVideoDownloader_v2.py
@@ -6,6 +6,8 @@ def Download(link):
     youtubeObject = YouTube(link).streams
     try:
         mp4streams = youtubeObject.filter(file_extension='mp4')
+       # !!! BUGFIX! if you get keyError [StreamingData], then go to C:\Users\<insertusername>\AppData\Local\Packages\PythonSoftwareFoundation.Python.3, etc, \LocalCache\local-packages\Python3XX\site-packages\pytube\innertube.py
+       #then, change line 78 to     def __init__(self, client='WEB', use_oauth=False, allow_cache=True): #(the client='ANDROID' should be client='WEB'
         print(mp4streams.get_highest_resolution())
         HighRes = mp4streams.get_highest_resolution()
         HighRes.download(output_path="D:/")
@@ -16,6 +18,7 @@ def Download(link):
 def AudioDownload(link):
     youtubeObject = YouTube(link).streams
     try:
+        #CHECK THE BUGFIX ABOVE
         mp4streams = youtubeObject.filter(file_extension='mp4')
         print(mp4streams.get_highest_resolution())
         HighRes = mp4streams.get_highest_resolution()

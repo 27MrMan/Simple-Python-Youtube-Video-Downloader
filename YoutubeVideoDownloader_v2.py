aa6e680ffff342ee15717476577ccdfeb70704f5
27MrMan <128210486+27MrMan@users.noreply.github.com>
2023-05-02 21:17:11 -0400

    Update YoutubeVideoDownloader_v2.py
    MAJOR changes, added a progress bar, and MUST change client from Andriod to WEB. More info soon...

--- YoutubeVideoDownloader_v2.py
+++ YoutubeVideoDownloader_v2.py
@@ -1,44 +1,33 @@
 from pytube import YouTube
+from pytube.cli import on_progress
 from moviepy.editor import *
 import time
-
-def Download(link):
-    youtubeObject = YouTube(link).streams
-    try:
-        mp4streams = youtubeObject.filter(file_extension='mp4')
-       # !!! BUGFIX! if you get keyError [StreamingData], then go to C:\Users\<insertusername>\AppData\Local\Packages\PythonSoftwareFoundation.Python.3, etc, \LocalCache\local-packages\Python3XX\site-packages\pytube\innertube.py
-       #then, change line 78 to     def __init__(self, client='WEB', use_oauth=False, allow_cache=True): #(the client='ANDROID' should be client='WEB'
-        print(mp4streams.get_highest_resolution())
-        HighRes = mp4streams.get_highest_resolution()
-        HighRes.download(output_path="D:/")
-    except:
-        print("Emotional Damage")
-    print("Download is completed successfully")
-
-def AudioDownload(link):
-    youtubeObject = YouTube(link).streams
-    try:
-        #CHECK THE BUGFIX ABOVE
-        mp4streams = youtubeObject.filter(file_extension='mp4')
-        print(mp4streams.get_highest_resolution())
-        HighRes = mp4streams.get_highest_resolution()
-        HighRes.download(output_path="D:/")
-        VideoNamed = YouTube(link).streams[0].title
-        VideoName = VideoNamed + ".mp4"
-        print(VideoName)
-        video = VideoFileClip(VideoName)
-        audio = video.audio
-        audio.write_audiofile(VideoName[:-3]+"mp3")
-    except:
-        print("Emotional Damage")
-    print("Download Completed Successfully")
-        
-LDigits = input("Enter the full youtube URL")
-link = LDigits
+    
+link = input("Enter the full youtube URL")
 SkipVideoDownload = input("Do you want to download the video as an MP4? Reply with Y or N")
 if  SkipVideoDownload == "Y":
-    Download(link)
+    yt = YouTube(link, on_progress_callback=on_progress)
+    ytstreams = yt.streams
+    mp4streams = ytstreams.filter(file_extension='mp4')
+    print(mp4streams.get_highest_resolution())
+    HighRes = mp4streams.get_highest_resolution()
+    HighRes.download(output_path="D:/")
+    print("Download is completed successfully")
+
 else:
-    AudioDownload(link)
+    yt = YouTube(link, on_progress_callback=on_progress)
+    ytstreams = yt.streams
+    mp4streams = ytstreams.filter(file_extension='mp4')
+    print(mp4streams.get_lowest_resolution(), "Download with start soon...")
+    lowres = mp4streams.get_lowest_resolution()
+    lowres.download(output_path="D:/")
+    VideoNamed = yt.title
+    VideoName = VideoNamed + ".mp4"
+    print(VideoName)
+    video = VideoFileClip(VideoName)
+    audio = video.audio
+    audio.write_audiofile(VideoName[:-3]+"mp3")
+print("Download Completed Successfully")
 print("Completed")
 time.sleep(5)
+#Line 78 of innertube.py in pytube folder has been changed from ANDRIOD to WEB. As soon as fix is found, fix it.

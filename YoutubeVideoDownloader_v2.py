0767079dd015c241f6bb21083c330ce36b320a25
27MrMan <128210486+27MrMan@users.noreply.github.com>
2023-08-05 09:54:35 -0400

    Update YoutubeVideoDownloader.py
    Optimised Code

--- YoutubeVideoDownloader_v2.py
+++ YoutubeVideoDownloader_v2.py
@@ -1,12 +1,11 @@
+#importing, if doesnt work, check readme
 from pytube import YouTube
 from pytube.cli import on_progress
 from moviepy.editor import *
 import time
-
-#Check bottom of file.
-
-link = input("Enter the full youtube URL")
-SkipVideoDownload = input("Do you want to download the video as an MP4? Reply with Y or N")
+    
+link = input("Enter the full youtube URL ")
+SkipVideoDownload = input("Do you want to download the video as an MP4? Reply with Y or N ")
 if  SkipVideoDownload == "Y":
     yt = YouTube(link, on_progress_callback=on_progress)
     ytstreams = yt.streams
@@ -16,22 +15,20 @@ if  SkipVideoDownload == "Y":
     HighRes.download(output_path="D:/")
     print("Download is completed successfully")
 
-else:
-    yt = YouTube(link)
+elif SkipVideoDownload == "N":
+    yt = YouTube(link, on_progress_callback=on_progress)
     ytstreams = yt.streams
     mp4streams = ytstreams.filter(file_extension='mp4')
+
     print(mp4streams.get_lowest_resolution())
     lowres = mp4streams.get_lowest_resolution()
     lowres.download(output_path="D:/", filename="tobeconverted.mp4")
     print("Download Completed Successfully")
-    FileName=input("What should the final file's name be? (NO SPACES) ")
-    VideoNamed = yt.title
-    print(VideoNamed)
     video = VideoFileClip(filename="tobeconverted.mp4")
-    audio = video.audio
-    audio.write_audiofile(filename=FileName+".mp3")
-print("Download Completed Successfully")
+    video.audio.write_audiofile(filename="convertedfile.wav", codec="pcm_s32le")
+
+else:
+    print(" Type 'Y' or 'N' ")
+    
 print("Completed")
 time.sleep(5)
-#Line 78 of innertube.py in pytube folder must be changed from ANDRIOD to WEB.
-#Progress bar does not work due to some issues with Pytube.

bf3cb11b8b07f43355fdc51986cb86bf6b32f9f7
27MrMan <128210486+27MrMan@users.noreply.github.com>
2023-08-06 11:08:34 +0530

    Update YoutubeVideoDownloader_v3.1.py
    Slight rewrite, the file conversion from .mp4 to .wav should work now.

--- YoutubeVideoDownloader_v2.py
+++ YoutubeVideoDownloader_v2.py
@@ -1,15 +1,14 @@
-#importing the various modules: "pytube" and "moviepy"
-
+#importing various modules...
 from pytube import YouTube
 from pytube.cli import on_progress
-from moviepy import editor
-from moviepy import video
+from moviepy.editor import *
 import time
 
+#program start 
 time.sleep(1)
 
 link = input("Enter the full youtube URL ")
-SkipVideoDownload = input("Do you want to download the video? Reply with Y, or N to split audio.")
+SkipVideoDownload = input("Do you want to download the video? Reply with Y or N, to split Audio. ")
 if  SkipVideoDownload == "Y":
     yt = YouTube(link, on_progress_callback=on_progress)
     ytstreams = yt.streams
@@ -26,12 +25,15 @@ elif SkipVideoDownload == "N":
 
     print(mp4streams.get_lowest_resolution())
     lowres = mp4streams.get_lowest_resolution()
-    lowres.download(output_path="D:/", filename="tobeconverted.mp4")
+    lowres.download(output_path="D:/", filename="Video+AudioDownload.mp4")
     print("Download Completed Successfully")
-    video2 = video.VideoFileClip(filename="tobeconverted.mp4")
-    video2.audio.write_audiofile(filename="convertedfile.wav", codec="pcm_s32le")
-else:
-    print("Type it exactly as 'Y' or 'N' (without the quotes)")
-print("Completed, closing...")
+    video = VideoFileClip(filename="D:/Video+AudioDownload.mp4")
+    audio = video.audio
+    audio.write_audiofile(filename="D:/AudioOnlyDownload.wav", codec="pcm_s32le")
+    print("File has been converted")
 
+else:
+    print("Type it exactly as 'Y' or 'N' (without the quotes). Run the program agian")
+
+print("Completed, closing...")
 time.sleep(5)
